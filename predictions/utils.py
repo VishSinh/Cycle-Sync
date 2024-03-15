74172cb843591dada1de64193436958aa5149777
Vishal Sinha <vish.sinh.vs@gmail.com>
2024-03-15 12:41:51 +0530

    Fixes in prediction

--- predictions/utils.py
+++ predictions/utils.py
@@ -1,6 +1,6 @@
 from django.db.models import Avg
 from django.utils import timezone
-import numpy as np
+from numpy import mean as np_mean, std as np_std, median as np_median
 from scipy.stats import norm
 
 from cycles.models import PeriodRecord
@@ -16,15 +16,15 @@ def calculate_cycle_lengths(user_id_hash):
 def get_average_cycle_length(user_id_hash):
     cycle_lengths = calculate_cycle_lengths(user_id_hash)
     if cycle_lengths:
-        return np.mean(cycle_lengths)
+        return np_mean(cycle_lengths)
     return None
 
 def get_period_statistics(user_id_hash):
     cycle_lengths = calculate_cycle_lengths(user_id_hash)
     if cycle_lengths:
-        average_cycle_length = np.mean(cycle_lengths)
-        std_dev_cycle_length = np.std(cycle_lengths)
-        median_cycle_length = np.median(cycle_lengths)
+        average_cycle_length = np_mean(cycle_lengths)
+        std_dev_cycle_length = np_std(cycle_lengths)
+        median_cycle_length = np_median(cycle_lengths)
         
         return {
             'average_cycle_length': average_cycle_length,
@@ -37,9 +37,11 @@ def predict_next_period_start(user_id_hash):
     cycle_lengths = calculate_cycle_lengths(user_id_hash)
     if cycle_lengths:
         # Assuming a normal distribution of cycle lengths
-        avg_cycle_length = np.mean(cycle_lengths)
-        std_dev_cycle_length = np.std(cycle_lengths)
-        # Predict next period start date based on average cycle length
+        avg_cycle_length = np_mean(cycle_lengths)
+        std_dev_cycle_length = np_std(cycle_lengths)
+        
+        # Predict next period start date based on average cycle length 
+        # TODO: FIX REQUIRED
         next_period_start_date = timezone.now() + timezone.timedelta(days=avg_cycle_length)
         # Calculate the probability of period starting within 7 days of the predicted date
         prob_within_7_days = norm.cdf(7, avg_cycle_length, std_dev_cycle_length)
