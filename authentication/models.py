8ffcabc5969e282e077474904a3235b810fd2155
Vishal Sinha <vish.sinh.vs@gmail.com>
2024-03-17 16:55:31 +0530

    Added Rate Limiting

--- authentication/models.py
+++ authentication/models.py
@@ -1,4 +1,3 @@
-from uuid import uuid4
 from djongo import models
 
 
@@ -8,4 +7,23 @@ class ActiveSessions(models.Model):
         
     user_id_hash = models.CharField(max_length=200, primary_key=True, editable=False)
     session_id = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class TrackerNonRegisteredUserRequests(models.Model):
+    class Meta:
+        db_table = 'tracker_non_registered_user_requests'
+        
+    ip_address = models.CharField(max_length=200, primary_key=True, editable=False)
+    count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class TrackerRegisteredUserRequests(models.Model):
+    class Meta:
+        db_table = 'tracker_registered_user_requests'
+        
+    user_id_hash = models.CharField(max_length=200, primary_key=True, editable=False)
+    ip_address = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
+    freq_count = models.IntegerField(default=0)
+    freq_req_at = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- period_tracking_BE/middlewares.py
+++ period_tracking_BE/middlewares.py
@@ -1,13 +1,14 @@
+from datetime import timedelta, timezone
 from django.conf import settings
-from django.core.cache import cache
-from django.http import JsonResponse
+from authentication.models import TrackerRegisteredUserRequests, TrackerNonRegisteredUserRequests
+from period_tracking_BE.helpers.utils import response_obj
+from rest_framework import status
 
 class RateLimitMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         
     def __call__(self, request):
-        
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         
         # Get the client's IP address
@@ -16,26 +17,43 @@ class RateLimitMiddleware:
             ip_address = x_forwarded_for.split(',')[0]
         else:
             ip_address = request.META.get('REMOTE_ADDR')
-            
-        print(ip_address)
+                        
+        user_id_hash = request.GET.get('user_id_hash')
+
+        if user_id_hash:
+            self.process_user_requests(user_id_hash, ip_address)
+        else:
+            tracker_requests, created = TrackerNonRegisteredUserRequests.objects.get_or_create(ip_address=ip_address)
+            tracker_requests.count += 1
+            tracker_requests.save()
+
+            if tracker_requests.count > settings.NON_REGISTERED_RATE_LIMIT:
+                return response_obj(success=False, message='Rate limit exceeded', status_code=status.HTTP_429_TOO_MANY_REQUESTS)
+
+        return self.get_response(request)
+    
+    
+    def process_user_requests(self, user_id_hash, ip_address):
+        tracker_request, created = TrackerRegisteredUserRequests.objects.get_or_create(
+            user_id_hash=user_id_hash,
+            defaults={'ip_address': ip_address}
+        )
+        # Update daily request count
+        tracker_request.count += 1
+
+        # Update the frequent request count
+        if tracker_request.freq_req_at and tracker_request.freq_req_at > timezone.now() - timedelta(minutes=settings.REGISTERED_FREQ_RATE_LIMIT_MINUTES):
+            tracker_request.freq_count += 1
+        else:
+            tracker_request.freq_count = 1
+            tracker_request.freq_req_at = timezone.now()
+
+        tracker_request.save()
+
+        if tracker_request.count > settings.REGISTERED_RATE_LIMIT or tracker_request.freq_count > settings.REGISTERED_FREQ_RATE_LIMIT:
+            return response_obj(success=False, message='Rate limit exceeded', status_code=status.HTTP_429_TOO_MANY_REQUESTS)
         
-        # Check if the client's IP address is rate limited and return an error if so   
-        # key = f'rate_limit:{ip_address}'
-        # count = cache.get(key)
         
-        # if count is not None and count >= settings.RATE_LIMIT:
-        #     return JsonResponse({'error': 'Rate limit exceeded'}, status=429)
-        
-        # # Increment the count for the client's IP address and set the expiration time
-        # if count is None:
-        #     timeout_seconds = settings.RATE_LIMIT_WINDOW.total_seconds()
-        #     cache.set(key, 1, timeout_seconds)
-        # else:
-        #     cache.incr(key)
-            
-        # cache.expire(key, settings.RATE_LIMIT_WINDOW.total_seconds())
-        
-        response = self.get_response(request)
-        return response
+
 
 
